0ec7761f0ebeeeddd6ec8e786a5e4a11670be843
João Pedro Souza <joaopedro150308@gmail.com>
2026-09-17 21:52:22 -0300

    Implementada estrutura de dados alunos

---
+++ TODO.md
@@ -0,0 +1,36 @@
+# Main
+
+- O sistema funcionará em um loop, para que o usuário escolha as opções.
+
+
+#  Interface
+onde o usuário poderá escolher entre as opções: cadastrar alunos, mostrar alunos cadastrados, remover alunos e sair. Usar o switch case. O programa só encerra caso o usuário selecione "sair".
+
+
+# Cadastro
+
+- Criar a estrutura de dados para os alunos (feito)
+- Criar o algorítimo de alocação de memória inicial para as estruturas (feito)
+- Criar o algorítimo de liberação de memória para as estruturas (feito)
+- Criar algorítimo de realocação de memória para as estruturas
+
+- Criar o algorítimo de preenchimento das estruturas (alunos)
+- Ler nome, criar uma matrícula válida, ler idade, definir turma.
+- Armazenar aluno no vetor de alunos.
+
+# Mostrar alunos 
+
+- Exibe os alunos cadastrados no vetor até o momento.
+
+
+# REMOVER
+
+- Removerá alunos da matriz de alunos. Para esse procedimento, é necessário que ao menos um aluno tenha sido cadastrado
+    
+
+    
+
+    
+
+    
+    
--- app/main.c
+++ app/main.c
@@ -1,30 +1,78 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NOME_TAMANHO 64
+#define INFOS_TAMANHO 20
+
+// É a quantidade inicial mínima de alunos.
+#define QUANT_INIT_ALUNOS 2
+
+void flush_in()
+{
+    int ch = 0;
+    do
+    {
+        ch = fgetc(stdin);
+    } while(ch != EOF && ch != '\n');
+}
+
 
 int main()
 {
-    printf("Primeiro commit\n");
+    // Definindo a estrutura dos alunos
+    char ***alunos = NULL;
 
-    /*
-    Interface do sistema: onde o usuário poderá escolher entre as opções: cadastrar alunos, mostrar alunos cadastrados, remover alunos e sair. Usar o switch case. O programa só encerra caso o usuário selecione "sair".
-    */
+    // Alocando memória para as estruturas
+    alunos = malloc(sizeof(char**) * QUANT_INIT_ALUNOS);
+    if(alunos == NULL) 
+    {
+        printf("Erro ao alocar memória.\n");
+        return 1;
+    }
 
-    /*
-    CADASTRO
-    Ler nome, criar uma matrícula válida, ler idade, definir turma.
-    Armazenar aluno no vetor de alunos.
-    */
+    for(int i = 0; i < QUANT_INIT_ALUNOS; i++)
+    {
+        alunos[i] = malloc(sizeof(char*) * 2); // Aloca espaço para o ponteiro matrz do aluno
+        if(alunos[i] == NULL)
+        {
+            return 1;
+        }
+        alunos[i][0] = malloc(NOME_TAMANHO); // Alocando espaço para o nome
+        alunos[i][1] = malloc(INFOS_TAMANHO); // Alocando espaço para as infos
 
-    /*
-    MOSTRAR ALUNOS
-    Exibe os alunos cadastrados no vetor até o momento.
-    */
+        // Algorítimo de liberação de memória caso haja erro
+        if(alunos[i][0] == NULL || alunos[i][1] == NULL)
+        {
+            for(int j = 0; j != i; j++)
+            {
+                free(alunos[j][0]);
+                free(alunos[j][1]);
+                free(alunos[j]);
+            }
+            free(alunos);
 
-    /*
-    REMOVER
-    Removerá alunos da matriz de alunos. Para esse procedimento, é necessário que ao menos um aluno tenha sido cadastrado
-    */
+            printf("Erro ao alocar memória.\n");
+            return 1;
+        }
+    }
+    // Fim alocação
 
+    // Pequena escrita dos alunos lidos (DEMO)
+    for(int i = 0; i < QUANT_INIT_ALUNOS; i++)
+    {
+        fgets(alunos[i][0], NOME_TAMANHO, stdin);
+        alunos[i][0][strcspn(alunos[i][0], "\n")] = '\0';
     
+        fgets(alunos[i][1], INFOS_TAMANHO, stdin);
+        alunos[i][1][strcspn(alunos[i][1], "\n")] = '\0';
+    }
+
+    for(int i = 0; i < QUANT_INIT_ALUNOS; i++)
+    {
+        printf("Nome: %s\tInfos: %s\n", alunos[i][0], alunos[i][1]);
+    }
+
 
 
     return 0;
